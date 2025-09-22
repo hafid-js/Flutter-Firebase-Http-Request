d650c1be7fb0c211770889121cd3e5e76fdd5a60
hafidjs <afidzpratama@gmail.com>
2025-09-22 18:58:21 +0700

    first commit

---
+++ lib - post/models/player.dart
@@ -0,0 +1,15 @@
+class Player {
+  String id;
+  String name;
+  String position;
+  String imageUrl;
+  DateTime createdAt;
+
+  Player({
+    required this.id,
+    required this.name,
+    required this.position,
+    required this.imageUrl,
+    required this.createdAt,
+  });
+}
